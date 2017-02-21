02a9d06afa768b5a811b52cee3e685059410963b
unknown <arekpl28@wp.pl>
2017-02-21 19:48:20 +0100

    Added some lamda in produkts

--- src/sda/finances/Application.java
+++ src/sda/finances/Application.java
@@ -5,6 +5,7 @@ import sda.finances.model.Expense;
 import sda.finances.model.Product;
 
 import java.lang.reflect.Array;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -31,20 +32,89 @@ public class Application {
 //                });
 //        System.out.println();
 //
-        expenses.forEach(expense -> {
-            expense.getProducts().stream()
-                    .filter(product -> product.getName().equals("banan"))
-                    .forEach(product -> System.out.println(product));
-        });
+//        double banan = expenses.stream()
+//                .mapToDouble(expense ->
+//                        expense.getProducts()
+//                                .stream()
+//                                .filter(product -> product.getName().equals("banan"))
+//                                .mapToDouble(product -> product.getUnitPrice() * product.getAmount())
+//                                .sum()
+//                )
+//                .sum();
+//        System.out.println(banan);
+//        System.out.println();
+//
+//        double spozywcze = expenses.stream()
+//                .filter(expense -> expense.getType().equals("spozywcze"))
+//                .mapToDouble(expense1 -> expense1.getPrice())
+//                .sum();
+//        System.out.println(spozywcze);
+//        System.out.println();
+//
+//        expenses.stream()
+//                .filter(expense -> expense.getDate().isBefore(LocalDate.of(2017, 2, 19)))
+//                .forEach(expense -> expense.getProducts()
+//                        .forEach(System.out::println));
+//        System.out.println();
+//6. Wyswietl wydatki dla konkretnego dnia
+        System.out.println(expenses.stream()
+                .filter(expense -> expense.getDate().isEqual(LocalDate.of(2017, 2, 21)))
+                .mapToDouble(expenses1 -> expenses1.getPrice())
+                .sum());
         System.out.println();
+        //Wyswietl wydatki dla konkretnego dnia na piwo
+        System.out.println(expenses.stream()
+                .filter(expense -> expense.getDate().isEqual(LocalDate.of(2017, 2, 21)))
+                .mapToDouble(expense -> expense.getProducts()
+                        .stream()
+                        .filter(product -> product.getName().equals("piwo"))
+                        .mapToDouble(product -> product.getUnitPrice() * product.getAmount())
+                        .sum())
+                .sum());
+        System.out.println();
+        //7. Zsumować całkowitą kwotę wydan a na produkty zaczynające się na p
 
-        expenses.stream()
-                .filter(expense -> expense.getType().equals("spozywcze"))
-                .forEach(expense -> {
-                    double price = expense.getPrice();
-                    System.out.println(price);
-                });
+        System.out.println(expenses.stream()
+                .mapToDouble(expense -> expense.getProducts()
+                        .stream()
+                        .filter(product -> product.getName().startsWith("p"))
+                        .mapToDouble(product -> product.getAmount() * product.getUnitPrice())
+                        .sum())
+                .sum());
         System.out.println();
+        //8. Zsumować koszt elementów spozywczych które kupiliśmy w parzystych ilościach
+
+        System.out.println(expenses.stream()
+                .filter(expense -> expense.getType().equals("spozywcze"))
+                .mapToDouble(expense -> expense.getProducts()
+                        .stream()
+                        .filter(product -> product.getAmount() % 2 == 0)
+                        .mapToDouble(produkt -> produkt.getAmount() * produkt.getUnitPrice())
+                        .sum())
+                .sum());
+        System.out.println();
+        //9. Z każdego expensa wyświetlić produkt za którego zapłaciliśmy najwięcej
+        expenses.stream()
+                .map(expense -> expense.getProducts()
+                        .stream()
+                        .max((e1, e2) ->
+                                (e1.getAmount() * e1.getUnitPrice()) >
+                                        (e2.getAmount() * e2.getUnitPrice()) ? 1 : -1)
+                        .get())
+                .forEach(product -> System.out.println(product));
+        System.out.println();
+        //10. Wyświetlić najdroższy produkt z wszystkich expensów
+        System.out.println(expenses.stream()
+                .map(expense -> expense.getProducts()
+                        .stream()
+                        .max((e1, e2) ->
+                                e1.getUnitPrice() >
+                                e2.getUnitPrice() ? 1 : -1)
+                        .get())
+                .max((e1, e2) ->
+                        e1.getUnitPrice() >
+                        e2.getUnitPrice() ? 1 : -1)
+                .get());
     }
 
 
@@ -60,7 +130,7 @@ public class Application {
         products2.add(new Product("farba", 1, 25));
         products2.add(new Product("mlotek", 2, 10));
         products2.add(new Product("gwozdzie", 100, 0.1));
-        Expense expense2 = new Expense("budowlane", products2, 2017, 2, 19);
+        Expense expense2 = new Expense("budowlane", products2);
 
         List<Product> products3 = new ArrayList<>();
         products3.add(new Product("witamina C", 2, 2));
